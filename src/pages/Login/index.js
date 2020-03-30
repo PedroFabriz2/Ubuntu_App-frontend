bc94efb6e1143726ad1f13dbdef5cc3c89a0e4b8
Pedro Henrique Fabriz Ulhoa <fabriz_pedro@MacBook-Air-de-Pedro.local>
2020-03-30 20:05:25 -0300

    new backend and frontend

---
+++ src/pages/Login/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+import { FiLogIn } from 'react-icons/fi';
+
+import LogoTreeImg from '../../assets/treeLogo.png';
+import LogoUbuntuImg from '../../assets/logoubuntu.png';
+
+
+export default function Login() {
+    return(
+        <div className="login-container">
+            
+                <section className="form">
+                    <form>
+                        <h1>Faça seu login</h1>
+
+                        <input placeholder="Sua ID"/>
+                        <button className="button" type='submit'>Entrar</button>
+
+                        <Link to="/cadastro">
+                            <FiLogIn size={24} color="#2A4B43"/>
+                            Não tenho cadastro
+                        </Link>
+                    </form>
+                </section>
+
+            <img src={LogoTreeImg} alt='TreeApp' />
+            <div className="imgUbuntu">
+                <img src={LogoUbuntuImg} alt='UbuntuImg' width="120px" height="134px" />
+            </div>
+            
+        </div>
+    );
+}

1a577ecc2b5d162859a810bd3575aba4fb9fb0c8
Pedro Henrique Fabriz Ulhoa <fabriz_pedro@MacBook-Air-de-Pedro.local>
2020-04-19 14:54:21 -0300

    ok2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ubuntutree.herokuapp.com/ongs',
+    baseURL: 'https://ubuntutree.herokuapp.com/',
 }) 
 
 export default api;
